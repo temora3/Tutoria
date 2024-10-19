8a66743d3ddd3e1cf369da8140584f1437d6164c
Sean Ratemo <seanratemo@Seans-MacBook-Pro.local>
2024-10-19 15:01:02 +0300

    First Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+import com.android.tools.r8.internal.br
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.example.tutoria"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.tutoria"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packagingOptions {
+        resources {
+            excludes += "META-INF/gradle/incremental.annotation.processors"
+        }
+    }
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    buildscript {
+        repositories {
+            google()
+            mavenCentral()
+            maven { url = uri("https://jitpack.io") }
+        }
+        dependencies {
+            classpath(libs.gradle)
+            classpath(libs.kotlin.gradle.plugin)
+            classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+            classpath(libs.hilt.android.gradle.plugin)
+        }
+    }
+}
+dependencies {
+    implementation(libs.hilt.android.v248)
+    implementation(libs.androidx.hilt.lifecycle.viewmodel)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.runtime)
+    implementation(libs.glide)
+    implementation(libs.circleimageview)
+    implementation("com.github.leandroborgesferreira:loading-button-android:2.3.0")
+    implementation ("it.xabaras.android:viewpagerindicator:2.0")
+    implementation("com.github.shuhart:stepview:1.5.1")
+    implementation(libs.hilt.android.compiler)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    buildscript {
+        repositories {
+            google()
+            mavenCentral()
+            maven { url = uri("https://jitpack.io") }
+        }
+        dependencies {
+            classpath(libs.gradle.v800)
+            classpath(libs.kotlin.gradle.plugin)
+            classpath(libs.androidx.navigation.safe.args.gradle.plugin.v253)
+            classpath(libs.hilt.android.gradle.plugin)
+        }
+    }
+}
