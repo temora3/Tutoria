4aa95222d2ad6c8b519f3202052903ce10b022bd
Charlene393 <131701102+Charlene393@users.noreply.github.com>
2024-11-01 20:43:14 +0300

    Authentication process

--- app/src/main/java/com/example/tutoria/LoginActivity.kt
+++ app/src/main/java/com/example/tutoria/LoginActivity.kt
@@ -1,16 +1,50 @@
 package com.example.tutoria
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tutoria.activities.TutoringActivity
+import com.example.tutoria.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.sign
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if(email.isNotEmpty()&& password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, TutoringActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.signupRedirectText.setOnClickListener{
+            val signUpIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signUpIntent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
