b6586b051ccc4eb4ec8ad5125641e1ddf2d7e0d0
Charlene393 <131701102+Charlene393@users.noreply.github.com>
2024-11-08 14:09:56 +0300

    fragment chat update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
