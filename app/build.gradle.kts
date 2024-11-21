34fcc89107315ede062c3d67dc35e88f12ce671d
Charlene393 <131701102+Charlene393@users.noreply.github.com>
2024-11-21 18:34:15 +0300

    Chat Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
